97eb997c78cc44290d8967b2ec262998c41f0abd
adhibarfan <adhib.arfan@gmail.com>
2016-10-19 11:32:26 +0700

    Update layout js

--- application/views/layout/JsLayout.php
+++ application/views/layout/JsLayout.php
@@ -2,13 +2,13 @@
  * @Author: adhibarfan
  * @Date:   2016-10-15 15:19:56
  * @Last Modified by:   adhibarfan
- * @Last Modified time: 2016-10-15 15:27:39
+ * @Last Modified time: 2016-10-19 11:30:48
  */ -->
- 	<!-- jQuery -->
+    <!-- jQuery -->
     <script src="<?php echo base_url(); ?>assets/js/jquery.min.js"></script>
 
     <!-- Bootstrap Core JavaScript -->
-    <script src="<?php echo base_url() ;?>assets/js/bootstrap.min.js"></script>
+    <script src="<?php echo base_url(); ?>assets/js/bootstrap.min.js"></script>
 
     <!-- Metis Menu Plugin JavaScript -->
     <script src="<?php echo base_url(); ?>assets/js/metisMenu.min.js"></script>
@@ -20,3 +20,6 @@
 
     <!-- Custom Theme JavaScript -->
     <script src="<?php echo base_url(); ?>assets/js/sb-admin-2.js"></script>
+
+    <script type="text/javascript" src="<?php echo base_url(); ?>assets/js/jquery.dataTables.min.js"></script>
+    <script type="text/javascript" src="<?php echo base_url(); ?>assets/js/dataTables.bootstrap.min.js"></script>
