d81876e186fbe6a19a98fb81aa12c581c998f169
adhibarfan <adhib.arfan@gmail.com>
2016-10-21 06:36:44 +0700

    Update Js layout

--- application/views/layout/JsLayout.php
+++ application/views/layout/JsLayout.php
@@ -4,22 +4,16 @@
  * @Last Modified by:   adhibarfan
  * @Last Modified time: 2016-10-19 11:30:48
  */ -->
-    <!-- jQuery -->
-    <script src="<?php echo base_url(); ?>assets/js/jquery.min.js"></script>
-
-    <!-- Bootstrap Core JavaScript -->
-    <script src="<?php echo base_url(); ?>assets/js/bootstrap.min.js"></script>
-
-    <!-- Metis Menu Plugin JavaScript -->
-    <script src="<?php echo base_url(); ?>assets/js/metisMenu.min.js"></script>
-
-    <!-- Morris Charts JavaScript -->
-    <script src="<?php echo base_url(); ?>assets/js/raphael.min.js"></script>
-    <script src="<?php echo base_url(); ?>assets/js/morris.min.js"></script>
-    <script src="<?php echo base_url(); ?>assets/js/morris-data.js"></script>
-
-    <!-- Custom Theme JavaScript -->
-    <script src="<?php echo base_url(); ?>assets/js/sb-admin-2.js"></script>
-
-    <script type="text/javascript" src="<?php echo base_url(); ?>assets/js/jquery.dataTables.min.js"></script>
-    <script type="text/javascript" src="<?php echo base_url(); ?>assets/js/dataTables.bootstrap.min.js"></script>
+<script type="text/javascript" src="<?php echo base_url(); ?>assets/js/jquery-3.1.0.min.js"></script>
+<script type="text/javascript" src="<?php echo base_url(); ?>assets/js/bootstrap.min.js"></script>
+<script type="text/javascript" src="<?php echo base_url(); ?>assets/js/jquery.dataTables.min.js"></script>
+<script type="text/javascript" src="<?php echo base_url(); ?>assets/js/dataTables.bootstrap.min.js"></script>
+<script type="text/javascript" src="<?php echo base_url(); ?>assets/js/jquery.datetimepicker.full.min.js"></script>
+<script type="text/javascript" src="<?php echo base_url(); ?>assets/js/autoNumeric.js"></script>
+<script type="text/javascript">
+    $(document).ready(function () {
+        $('.angka').autoNumeric("init", {
+            aSign: 'Rp '
+        });
+    });
+</script>
