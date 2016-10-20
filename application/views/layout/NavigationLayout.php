b5f2d57748cd51ca7ef3ce2659f3a8a0daa33f48
adhibarfan <adhib.arfan@gmail.com>
2016-10-21 06:32:18 +0700

    Update tampilan layout navigasi

--- application/views/layout/NavigationLayout.php
+++ application/views/layout/NavigationLayout.php
@@ -2,37 +2,65 @@
  * @Author: adhibarfan
  * @Date:   2016-10-15 19:07:33
  * @Last Modified by:   adhibarfan
- * @Last Modified time: 2016-10-15 19:34:56
+ * @Last Modified time: 2016-10-19 11:53:44
  */
- -->
+-->
 
-            <div class="navbar-default sidebar" role="navigation">
-                <div class="sidebar-nav navbar-collapse">
-                    <ul class="nav" id="side-menu">
-                        <li class="sidebar-search">
-                            <div class="input-group custom-search-form">
-                                <input type="text" class="form-control" placeholder="Search...">
-                                <span class="input-group-btn">
-                                <button class="btn btn-default" type="button">
-                                    <i class="fa fa-search"></i>
-                                </button>
-                            </span>
-                            </div>
-                            <!-- /input-group -->
-                        </li>
-                        <li>
-                            <a href="<?php echo base_url(); ?>index.php/admin/dashboard"><i class="fa fa-dashboard fa-fw"></i> Dashboard</a>
-                        </li>
+<nav class="navbar navbar-default navbar-static-top" role="navigation" style="margin-bottom: 0">
+    <div class="navbar-header">
+        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+        </button>
+        <a class="navbar-brand" href="<?php echo base_url();?>/index.php/admin/dashboard">Data Mahasiswa</a>
+    </div>
+    <!-- /.navbar-header -->
 
-                        <li>
-                            <a href="<?php echo base_url(); ?>index.php/admin/mahasiswa/"><i class="fa fa-table fa-fw"></i> Data Mahasiswa</a>
-                        </li>
-                        <li>
-                            <a href="#"><i class="fa fa-edit fa-fw"></i> Data Buku</a>
-                        </li>
+    <ul class="nav navbar-top-links navbar-right">
+        <li class="dropdown">
+            <a class="dropdown-toggle" data-toggle="dropdown" href="#">
+                <i class="fa fa-user fa-fw"></i> Nama User <i class="fa fa-caret-down"></i>
+            </a>
+            <ul class="dropdown-menu dropdown-user">
+                <li><a href="#"><i class="fa fa-sign-out fa-fw"></i> Logout</a>
+                </li>
+            </ul>
+            <!-- /.dropdown-user -->
+        </li>
+        <!-- /.dropdown -->
+    </ul>
+    <!-- /.navbar-top-links -->
 
-                    </ul>
-                </div>
-                <!-- /.sidebar-collapse -->
-            </div>
-                    </nav>
+    <div class="navbar-default sidebar" role="navigation">
+        <div class="sidebar-nav navbar-collapse">
+            <ul class="nav" id="side-menu">
+                <li class="sidebar-search">
+                    <div class="input-group custom-search-form">
+                        <input type="text" class="form-control" placeholder="Search...">
+                        <span class="input-group-btn">
+                            <button class="btn btn-default" type="button">
+                                <i class="fa fa-search"></i>
+                            </button>
+                        </span>
+                    </div>
+                    <!-- /input-group -->
+                </li>
+
+                <li>
+                    <a href="<?php echo base_url(); ?>index.php/admin/dashboard"><i class="fa fa-dashboard fa-fw"></i> Dashboard</a>
+                </li>
+
+                <li>
+                    <a href="<?php echo base_url(); ?>index.php/admin/mahasiswa/"><i class="fa fa-table fa-fw"></i> Data Mahasiswa</a>
+                </li>
+                <li>
+                    <a href="#"><i class="fa fa-edit fa-fw"></i> Data Buku</a>
+                </li>
+            </ul>
+        </div>
+        <!-- /.sidebar-collapse -->
+    </div>
+    <!-- /.navbar-static-side -->
+</nav>
